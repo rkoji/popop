2af00e9c5eccd7fe82afd3d99e758d9b65bb5577
jiyeon <wl0wn1212@gmail.com>
2024-01-12 09:51:00 +0900

    Test : 사용자 로그인 test

--- popop/src/test/java/com/example/popop/domain/user/controller/UserControllerTest.java
+++ popop/src/test/java/com/example/popop/domain/user/controller/UserControllerTest.java
@@ -1,24 +1,31 @@
 package com.example.popop.domain.user.controller;
 
 import com.example.popop.domain.user.dto.UserDto;
+import com.example.popop.domain.user.dto.UserLoginDto;
 import com.example.popop.domain.user.entity.Role;
 import com.example.popop.domain.user.entity.User;
 import com.example.popop.domain.user.repository.UserRepository;
-import org.assertj.core.api.Assertions;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class UserControllerTest {
 
     @LocalServerPort
@@ -29,6 +36,14 @@ class UserControllerTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private static String url = "http://localhost:";
 
     @Test
     void User_회원가입() throws Exception {
@@ -42,7 +57,7 @@ class UserControllerTest {
                 .role(Role.USER)
                 .build();
 
-        String url = "http://localhost:"+port+"/signup";
+        url = url + port + "/signup";
 
         // when
         ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, userDto, Long.class);
@@ -56,6 +71,48 @@ class UserControllerTest {
     }
 
     @Test
-    void userLogin() {
+    void User_로그인_성공() throws Exception {
+        // given
+        UserLoginDto userLoginDto = UserLoginDto.builder()
+                .loginId("id")
+                .password("re123")
+                .build();
+
+        // when
+        mockMvc.perform(MockMvcRequestBuilders.post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userLoginDto)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+    }
+
+    @Test
+    void User_로그인_실패_잘못된_패스워드() throws Exception {
+        // given
+        UserLoginDto userLoginDto = UserLoginDto.builder()
+                .loginId("id")
+                .password("wrongPassword")
+                .build();
+
+        // when
+        mockMvc.perform(MockMvcRequestBuilders.post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userLoginDto)))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    void User_로그인_실패_존재하지_않는_아이디() throws Exception {
+        // given
+        UserLoginDto userLoginDto = UserLoginDto.builder()
+                .loginId("nonexistentId")
+                .password("somePassword")
+                .build();
+
+        // when
+        mockMvc.perform(MockMvcRequestBuilders.post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userLoginDto)))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 }
